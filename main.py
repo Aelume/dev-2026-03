4350faf56dd78461a7d2cccebd24c080b14af68c
Aelume <a2696938949@hotmail.com>
2026-04-20 00:15:31 +0800

    1.1.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent
-DATA_DIR = BASE_DIR / "data"
+DATA_DIR = BASE_DIR / "scripts" / "data"
 OUTPUT_DIR = BASE_DIR / "output"
 
 
